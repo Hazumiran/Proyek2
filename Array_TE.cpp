6da50c4c11baec5b4dc8c43e35866e8435069096
unknown <bennyyoga77@gmail.com>
2022-03-13 13:51:30 +0700

    Nambahin yang array

---
+++ Array_TE.cpp
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+#define LEN 80
+
+char text[MAX][LEN];
+void arr();
+int edit();
+void Total_Char();
+void display();
+
+int main(void)
+{
+	printf("\t1.Creat\n\t2.Tampil\n");
+	int pil;
+	scanf("%i",&pil);
+  	do{
+  		switch (pil){
+  			case 1:{
+  				printf("masukkan data kosong untuk quit.\n");
+		  		arr();
+				break;
+			}
+			case 2:{
+				display();
+				break;
+			}
+			case 3:{
+				Total_Char();
+				break;
+			}
+		}
+	}while(pil != 4);
+	
+	end1:;
+}
+
+void arr(){
+	
+	start1:
+	char pil;
+	register int t, i, j;
+	fflush(stdin);
+	
+  	for(t = 0; t < MAX; t++){
+	    printf("%d: ", t);
+	    gets(text[t]);
+	    if(!*text[t]) 
+	        break; /* quit on blank line */
+	}
+	
+	printf("\nIsi teks\n");
+	for(i = 0; i < t; i++) {
+	   for(j = 0; text[ i ][ j ]; j++) 
+	       putchar(text[ i ][ j ]);
+	 	  putchar('\n');
+	}
+	
+	printf("\nApakah Akan kembali Menulis y/t : ");
+	scanf("%c",&pil);
+	
+	if(pil == 'y' || pil == 'Y'){
+		goto start1;
+	}
+	else{
+		main();
+	}
+}
+
+void Total_Char(){
+	int total;
+	int i,j,t;
+	
+	for(i = 0; i < MAX; i++) {
+	   for(j = 0; text[ i ][ j ]; j++) {
+	       total+=1;
+		}
+	 	
+		if(!*text[i]) 
+	    	break;
+	}
+	printf("\nTotal Character : %i",total);
+	printf("\nTotal Baris	 : %i\n\n",i);
+	main();
+}
+
+void display(){
+	
+	register int t, i, j;
+	
+	printf("\nIsi teks\n");
+	for(i = 0; i < t; i++) {
+	   for(j = 0; text[ i ][ j ]; j++) 
+	       putchar(text[ i ][ j ]);
+	 	  putchar('\n');
+	}
+	
+	main();
+}
